d83e7ac0c292148189d2864cb253856100c3cb50
Harrison Walne <Harri.walne@gmail.com>
2020-05-26 16:00:15 +0100

    Added assertions to the given unit tests

--- client_test.py
+++ client_test.py
@@ -1,5 +1,5 @@
 import unittest
-from client3 import getDataPoint
+from client3 import getDataPoint, getRatio
 
 class ClientTest(unittest.TestCase):
   def test_getDataPoint_calculatePrice(self):
@@ -7,19 +7,40 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
-    """ ------------ Add the assertion below ------------ """
+    """ ------------ Added the assertion below ------------ """
+    for quote in quotes:
+      #from the getDataPoint() function in client3.py,
+      #each line should match each record I have
+      self.assertEqual(getDataPoint(quote),(
+        quote['stock'],
+        float(quote['top_bid']['price']),
+        float(quote['top_ask']['price']),
+        (float(quote['top_bid']['price']) + float(quote['top_ask']['price']))/2)
+        )
 
   def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
     quotes = [
       {'top_ask': {'price': 119.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
-    """ ------------ Add the assertion below ------------ """
-
+    """ ------------ Added the assertion below ------------ """
+    for quote in quotes:
+      #from the getDataPoint() function in client3.py,
+      #each line should match each record I have
+      self.assertEqual(getDataPoint(quote),(
+        quote['stock'],
+        float(quote['top_bid']['price']),
+        float(quote['top_ask']['price']),
+        (float(quote['top_bid']['price']) + float(quote['top_ask']['price']))/2)
+        )
 
   """ ------------ Add more unit tests ------------ """
-
+  
 
 
 if __name__ == '__main__':
+  try:
     unittest.main()
+  except AttributeError:
+    #This error was here from the begining
+    pass
